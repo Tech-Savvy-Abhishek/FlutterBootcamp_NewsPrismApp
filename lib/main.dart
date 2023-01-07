5db1256939b2519dfa1c71d1ce208deb6330e95d
Tech-Savvy-Abhishek <abhishekgs999@outlook.com>
2023-01-07 20:12:53 +0530

    Added splash screen activity

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:news_prism/splash.dart';
 
 import 'home.dart';
 
 void main() {
   runApp(MaterialApp(initialRoute: "/",
       routes: {
-    "/": (context) => Home(),
+    "/": (context) => Splash(),
   }),
   );
 }
---
+++ lib/splash.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    navToHome();
+  }
+
+  navToHome() async
+  {
+      await Future.delayed(Duration(milliseconds: 1500),(){});
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Home()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF0A2647),
+      body: Center(
+        child: Container(
+          child: Image.asset("images/splash_image.jpg"),
+        ),
+      ),
+    );
+
+  }
+}
