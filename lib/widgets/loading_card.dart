06dd2161937becad65b72e56b7f16dc6dfec8239
Tech-Savvy-Abhishek <abhishekgs999@outlook.com>
2023-01-31 17:52:32 +0530

    Modified shimmer effect loading

--- lib/widgets/loading_card.dart
+++ lib/widgets/loading_card.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class LoadingCard extends StatelessWidget {
   const LoadingCard({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 0, vertical: 5),
-      height: 250,
-      width: double.infinity,
-      padding: EdgeInsets.all(0.8),
-      decoration: BoxDecoration(
-        color: Colors.black.withOpacity(0.3),
-        borderRadius: BorderRadius.all(Radius.circular(16)),
+    return Shimmer.fromColors(
+      baseColor: Color(0xFF808080),
+      highlightColor: Color(0xFFC2C2C2),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 0, vertical: 5),
+        height: 250,
+        width: double.infinity,
+        padding: EdgeInsets.all(0.8),
+        decoration: BoxDecoration(
+          color: Colors.black.withOpacity(0.3),
+          borderRadius: BorderRadius.all(Radius.circular(16)),
+        ),
       ),
     ); //Card
   }
